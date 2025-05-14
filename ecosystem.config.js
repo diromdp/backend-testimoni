3d5c3044df093a8f1e32a970905e97c8493678cd
unknown <dirom@catalyzecommunications.com>
2025-05-14 10:41:21 +0700

    add PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+      name: 'nestjs-alfa',
+      script: 'dist/main.js', // Path to your built main file
+      cwd: '/var/www/backend-testimoni',
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3001
+      }
+    }]
+  };
