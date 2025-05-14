8388308cea6ba6c1bff92f15f2b4964a8dcaef74
unknown <dirom@catalyzecommunications.com>
2025-05-14 18:54:48 +0700

    add logger

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   process.env.TZ = 'Asia/Jakarta';
 
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule,
+    {
+      cors: true,
+      logger: ['error', 'warn', 'log', 'verbose', 'debug']
+    }
+  );
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
