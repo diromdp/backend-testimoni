b95b4d42c069e7908fd0c422192740f5e9bcbcbc
unknown <dirom@catalyzecommunications.com>
2025-05-14 10:43:57 +0700

    Update PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
       name: 'nestjs-alfa',
-      script: 'dist/main.js', // Path to your built main file
+      script: 'dist/src/main.js', // Path to your built main file
       cwd: '/var/www/backend-testimoni',
       env_production: {
         NODE_ENV: 'production',
