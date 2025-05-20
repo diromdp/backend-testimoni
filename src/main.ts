5c26fc2022fc029e10d2ba736f992e2db959945e
unknown <dirom@catalyzecommunications.com>
2025-05-20 11:11:04 +0700

    cors false

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   process.env.TZ = 'Asia/Jakarta';
 
-  const corsOptions = {
-    origin: '*', // Mengizinkan semua origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // Jika Anda menggunakan cookies atau header Authorization
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  };
-
   const app = await NestFactory.create(AppModule,
     {
-      cors: corsOptions,
+      cors: false,
       logger: ['error', 'warn', 'log', 'verbose', 'debug']
     }
   );
