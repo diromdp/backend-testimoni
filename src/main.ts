5545f93e914eb5a6393ab77e251040eb7d2652d5
unknown <dirom@catalyzecommunications.com>
2025-05-20 10:57:19 +0700

    change cross

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   process.env.TZ = 'Asia/Jakarta';
 
+  const corsOptions = {
+    origin: [
+      'https://syafaq-fe.vercel.app', // Domain frontend Anda
+      'https://syafaq.com',
+      'http://localhost:3000', // Contoh untuk pengembangan frontend lokal
+      'http://127.0.0.1:3000', // Contoh lain untuk pengembangan frontend lokal
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Jika Anda menggunakan cookies atau header Authorization
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
+
   const app = await NestFactory.create(AppModule,
     {
-      cors: true,
+      cors: corsOptions,
       logger: ['error', 'warn', 'log', 'verbose', 'debug']
     }
   );
