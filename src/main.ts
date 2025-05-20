039d2719fb89ab86cbf087d3c9291ac1b4a9aa5d
unknown <dirom@catalyzecommunications.com>
2025-05-20 11:23:08 +0700

    Fixing CROSS

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   process.env.TZ = 'Asia/Jakarta';
 
-  const app = await NestFactory.create(AppModule,
-    {
-      cors: false,
-      logger: ['error', 'warn', 'log', 'verbose', 'debug']
-    }
-  );
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'verbose', 'debug']
+  });
+  
+  // Konfigurasi CORS secara eksplisit
+  app.enableCors({
+    origin: true, // Mengizinkan semua origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  
   await app.listen(5019);
 }
 bootstrap();
